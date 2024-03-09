4b52478b2faa8538b6fed9fa6168c2160e12bf25
Test <irischj@mail.ru>
2024-03-09 09:58:25 +0500

    src/

--- src/main/java/ru/netology/javaqa/dz_5_batchesAndLoops/services/RestService.java
+++ src/main/java/ru/netology/javaqa/dz_5_batchesAndLoops/services/RestService.java
@@ -6,11 +6,8 @@ public class RestService {
         int money = 0; // количество денег на счету
 
         for (int month = 1; month <= 12; month++) {
-
             if (money < threshold) { // можем ли отдыхать?
-
                 money = money + income - expenses;
-
             } else {
                 money = (money - expenses) / 3;
                 count++;
